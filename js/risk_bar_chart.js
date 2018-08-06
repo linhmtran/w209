afabcc9e01acaf18b4639e50f25fe9f776a10ec2
Mark <mgin@cryptum.T-mobile.com>
2018-08-05 23:08:12 -0700

    Cleanup PCF graph, risk chart basics

--- js/risk_bar_chart.js
+++ js/risk_bar_chart.js
@@ -1,166 +1,166 @@
-// needs global selectedCounty
-var pcfStats;
-var selectedCounty = 'CA_STATE';
 
 // risk bar chart w/ref lines svg
 //Set dimensions
-var r = { top: 50, right: 50, bottom: 120, left: 70 }, h = 550 - r.top - r.bottom, w = 500 - r.left - r.right, barWidth = 10;
+var r_m = { top: 50, right: 50, bottom: 70, left: 70 }, r_h = 400 - r_m.top - r_m.bottom, r_w = 200 - r_m.left - r_m.right, r_barWidth = 10;
 
 //Draw svg
-var bar_svg = d3.select('#bar_chart_ref').append('svg')
-        .attr('width', w + r.left + r.right)
-        .attr('height', h + r.top + r.bottom)
+var r_svg = d3.select('#risk_chart').append('svg')
+        .attr('width', r_w + r_m.left + r_m.right)
+        .attr('height', r_h + r_m.top + r_m.bottom)
         .append('g')
-        .attr('transform', 'translate(' + r.left + ',' + r.top + ')');
+        .attr('transform', 'translate(' + r_m.left + ',' + r_m.top + ')');
 
 //Axes and scales
-var xScale = d3.scaleBand()
+var r_xScale = d3.scaleBand()
 .domain(['Fatal', 'Injury'])
-.range([0, w])
+.range([0, r_w])
 .round(.1);
 //.range(['#8b0000', '#cccc00', '#e69500']);
 // domain and range can be fixed
 
-var yScale = d3.scaleLinear()
-.range([h, 0]);
+var r_yScale = d3.scaleLinear()
+.range([r_h, 0]);
 
-var xAxis = d3.axisBottom()
-          .scale(xScale);
+var r_xAxis = d3.axisBottom()
+          .scale(r_xScale);
 
-var yAxis = d3.axisLeft()
-          .scale(yScale);
+var r_yAxis = d3.axisLeft()
+          .scale(r_yScale);
 
 //Draw axes
-bar_svg.append('g')
+r_svg.append('g')
 .attr('class', 'x axis')
-.attr('transform', 'translate(0,' + h + ')')
-.call(xAxis);
+.attr('transform', 'translate(0,' + r_h + ')')
+.call(r_xAxis);
 
-bar_svg.append('g')
+r_svg.append('g')
 .attr('class', 'y axis')
-.call(yAxis);
+.call(r_yAxis);
 
-
-// get collision counts/types for the global selectedCounty
-var getCountyCollisions = function (data) {
-  var c_search = 'Total';
-  if (selectedCounty != '') {
-    c_search = selectedCounty;
+// sum up the injury/fatality across all pcf categories for a selected county
+var getCountyTotalRiskCounts = function (county, data) {
+  var stats = { Injury: 0,
+                Fatal: 0,
+                Property: 0,
+                Total: 0
   }
-
-  var cData;
   data.forEach(function (d) {
-    if (d.County == c_search) {
-      cData = d.CollisionFactor;
-    }
+    if (d.County == county) {
+      var cfData = d.CollisionFactor;
+      // console.log(Object.keys(cfData));
+      Object.keys(cfData).forEach(function (pcf) {
+        // console.log(pcf);
+        stats.Injury += cfData[pcf].Injury;
+        stats.Fatal += cfData[pcf].Fatal;
+        stats.Property += cfData[pcf].Property;
+        stats.Total += cfData[pcf].Total;
 
-    var sumInjuryForCounty = function (county, data) {
-      var injury = 0;
-      var fatal = 0;
-      var property = 0;
-      var total = 0;
-      data.forEach(function (d) {
-        if (d.County == county) {
-          var cfData = d.CollisionFactor;
-          console.log(Object.keys(cfData));
-          Object.keys(cfData).forEach(function (pcf) {
-            console.log(pcf);
-            injury += cfData[pcf].Injury;
-            fatal += cfData[pcf].Fatal;
-            property += cfData[pcf].Property;
-            total += cfData[pcf].Total;
-          });
-        }
       });
-
-   console.log('injured = ' + injury);
-      console.log('fatal = ' + fatal);
-      console.log('property = ' + property);
-      console.log('total = ' + total);
-    };
-  });
-
-  return cData;
-};
-
-// get the aggregate total collisions for global selectedCounty
-var getCountyInjuryTotals = d3.nest()
-  .key(function (d) { return d.selectedCounty; })
-  .rollup(function (v) { return {
-    total: d3.sum(v, function (d) { return d.amount; }),
-  }; });
-
-// get the max number of collisions of any type for the global selectedCounty
-var getMaxCollisionTypeValue = function (data) {
-  var vMax = 0;
-  var cData = getCountyCollisions(data);
-  Object.keys(cData).forEach(function (key) {
-    //console.log(cData[key]);
-    if (vMax < cData[key]) {
-      vMax = cData[key];
     }
   });
+  // console.log(stats);
+  return stats;
+}
 
-  console.log('max = ' + vMax);
-  return vMax;
-};
+// get the injury/fatality stats for a particular county and pcf category
+var getCountyPCFRiskCounts = function (county, pcf, data) {
+ var stats;
+ data.forEach(function(d) {
+   if(d.County == county) {
+     // console.log("county = " + county);
+     // console.log("d = " + d.CollisionFactor[pcf]);
+     // console.log(d.CollisionFactor);
+     stats = d.CollisionFactor[pcf];
+   }
+ });
+ return stats;
+}
+
+// calculate and return an object which contains the "average" risk rates
+// for injuries, fatalities, and property across all pcf categories for a specific county
+var getCountyTotalRiskRate = function (county, data) {
+  var totalStats = getCountyTotalRiskCounts(county, data);
+  var rates = {Injury: totalStats.Injury/totalStats.Total,
+               Fatal: totalStats.Fatal/totalStats.Total,
+               Property: totalStats.Property/totalStats.Total
+  };
+  return rates;
+}
+
+// calculate and return an object which contains the risk rate
+// for injury, fatalities, and property only for a specific PCF category for a county
+var getCountyPCFRiskRate = function (county, pcf, data) {
+  var stats = getCountyPCFRiskCounts(county, pcf, data);
+  var rates = { Injury: stats.Injury/stats.Total,
+                Fatal: stats.Fatal/stats.Total,
+                Property: stats.Property/stats.Total
+  };
+  return rates;
+}
 
 var updateRiskBarGraph = function (data) {
   // clear title and labels
-  bar_svg.selectAll('text')
-   .remove();
+  r_svg.selectAll('text')
+    .remove();
 
-  // county data
-  // console.log(pcfStats);
-  var countyData = getCountyCollisions(data);
+  console.log(getCountyTotalRiskRate("Los Angeles", data));
+
+  console.log(getCountyPCFRiskRate("Los Angeles", "DUI", data));
+  console.log(getCountyPCFRiskRate("Los Angeles", "Unsafe Speed", data));
+
+  console.log(getCountyTotalRiskRate("Alpine", data));
+  console.log(getCountyPCFRiskRate("Alpine", "DUI", data));
+
+  var totalRiskRates = getCountyTotalRiskRate(selectedCounty, data);
+  console.log(Object.keys(totalRiskRates));
 
   // update bar width for number of collision types
-  barWidth = w / Object.keys(countyData).length;
+  r_barWidth = r_w / (Object.keys(totalRiskRates).length);
 
-  // update domain y axis
-  yScale.domain([0, getMaxCollisionTypeValue(data)]);
+  // update domain y axis, we know it's 0 to 1 since dealing with rates
+  r_yScale.domain([0, 1]);
 
   // update domain x axis
-  xScale.domain(Object.keys(countyData).map(function (d) {
+  r_xScale.domain(Object.keys(totalRiskRates).map(function (d) {
     // console.log ("x domain " + d)
     return d;
   }));
 
   // define the tooltip
-  var div = d3.select('#bar_chart_ref').append('div')
-  .attr('class', 'tooltip')
-  .style('opacity', 0);
+  // var div = d3.select('#bar_chart_ref').append('div')
+  // .attr('class', 'tooltip')
+  // .style('opacity', 0);
 
   //select all bars on the graph, take them out, and exit the previous data set.
   //then you can add/enter the new data set
-  var bars = bar_svg.selectAll('.bar')
+  var bars = r_svg.selectAll('.bar')
   .remove()
      .exit()
-     .data(Object.keys(countyData));
+     .data(Object.keys(totalRiskRates));
   //now actually give each rectangle the corresponding data
   bars.enter()
   .append('rect')
   .attr('class', 'bar')
   .attr('x', function (d, i) {
-    return xScale(d) + 2;
+    return r_xScale(d) + 2;
   })
   .attr('y', function (d, i) {
-    return yScale(countyData[d]);
+    return r_yScale(totalRiskRates[d]);
   })
   .attr('height', function (d, i) {
-    return h - yScale(countyData[d]);
+    return r_h - r_yScale(totalRiskRates[d]);
   })
-  .attr('width', barWidth - 4)
+  .attr('width', r_barWidth - 4)
   .attr('fill', 'steelblue')
      .on('mouseover', function (d) {
-    div.transition()
-   .duration(200)
-   .style('opacity', 0.9);
-    div.html('Collision Factor: ' + d + '<br/>' +
-           'Number of Collisions: ' + countyData[d])
-   .style('left', (d3.event.pageX) + 'px')
-   .style('top', (d3.event.pageY) + 'px');
-  })
+        div.transition()
+        .duration(200)
+        .style('opacity', 0.9);
+        div.html('Risk: ' + d + '<br/>' +
+                'Rate: ' + totalRiskRates[d])
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY) + 'px');
+      })
   .on('mouseout', function (d) {
     div.transition()
     .duration(500)
@@ -168,21 +168,21 @@ var updateRiskBarGraph = function (data) {
   });
 
   // update axes
-  bar_svg.selectAll('g')
+  r_svg.selectAll('g')
    .remove();
-  bar_svg.append('g')
+  r_svg.append('g')
       .attr('class', 'x axis')
-      .attr('transform', 'translate(0,' + h + ')')
-      .call(xAxis)
+      .attr('transform', 'translate(0,' + r_h + ')')
+      .call(r_xAxis)
       .selectAll('text')
        .style('text-anchor', 'end')
        .attr('dx', '-.8em')
        .attr('dy', '.15em')
        .attr('transform', 'rotate(-45)');
 
-  bar_svg.append('g')
+  r_svg.append('g')
       .attr('class', 'y axis')
-      .call(yAxis)
+      .call(r_yAxis)
       .append('text')
        .attr('transform', 'rotate(-90)')
        .attr('y', 6)
@@ -190,23 +190,23 @@ var updateRiskBarGraph = function (data) {
        .style('text-anchor', 'end');
 
   // title showing county selection
-  bar_svg.append('text')
+  r_svg.append('text')
    // .attr("x", w/2)
    // .attr("y", 0)
-   .attr('transform', 'translate(' + w / 2 + ',-' + r.top / 2 + ')')
+   .attr('transform', 'translate(' + r_w / 2 + ',-' + r_m.top / 2 + ')')
    .style('text-anchor', 'middle')
    .style('font-size', '14px')
    .style('text-decoration', 'bold')
-   .text('Number of Collisions by Causes for Selected Region: ' + selectedCounty);
+   .text('Risk Rates for Selected County: ' + selectedCounty);
 
   // y axis label
-  bar_svg.append('text')
-   .attr('transform', 'translate(-50,' + (h / 2 + r.top) + ') rotate(-90)')
-   .text('Number of Collisions');
+  r_svg.append('text')
+   .attr('transform', 'translate(-50,' + (r_h / 2 + r_m.top) + ') rotate(-90)')
+   .text('Rate');
   // x axis label
-  bar_svg.append('text')
-   .attr('transform', 'translate(' + (w / 2 - r.left) + ',' + (h + r.bottom - 5) + ')')
-   .text('Primary Collision Factor');
+  r_svg.append('text')
+   .attr('transform', 'translate(' + (r_w / 2 - r_m.left) + ',' + (r_h + r_m.bottom - 5) + ')')
+   .text('Risk Category');
 
 }; // end updateRiskBarGraphGraph
 
@@ -216,7 +216,7 @@ d3.queue()
 	.awaitAll(function (error, results) {
 		if (error) throw error;
 
-  pcfStats = results[0];
-  //console.log(pcfStats);
-  updateRiskBarGraph(pcfStats);
+  var injuryStats = results[0];
+  console.log(injuryStats);
+  updateRiskBarGraph(injuryStats);
 });
